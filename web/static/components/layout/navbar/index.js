388678727ae890f7cd8173eb2b28a6ae1d3e55c6
wangcd <ethan522@hotmail.com>
2024-03-13 10:55:07 +0800

    Change update url

--- web/static/components/layout/navbar/index.js
+++ web/static/components/layout/navbar/index.js
@@ -526,7 +526,7 @@ export class LayoutNavbar extends CustomElement {
     this.layout_userpris = navbar_list.map((item) => (item.name));
     this._active_name = "";
     this._update_appversion = "";
-    this._update_url = "https://github.com/NAStool/nas-tools";
+    this._update_url = "https://github.com/ethan522/nas-tools";
     this._is_update = false;
     this.classList.add("navbar","navbar-vertical","navbar-expand-lg","lit-navbar-fixed","lit-navbar","lit-navbar-hide-scrollbar");
   }
